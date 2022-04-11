0992aaf7858afadf20860582fef83b9cff12be1c
Fabrix300 <fabrix3434@gmail.com>
2022-04-11 14:50:51 -0500

    avance de HomeComponent | falta mejorar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { InvestmentOffer } from '../data/interfaces/investmentOffer';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +10,34 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService: Title) { }
+  newInvOffers: InvestmentOffer[] = [];
+  numbers: number[] = [1,2,3];
+
+  constructor(private titleService: Title, private homeService: HomeService) { }
 
   ngOnInit(): void {
     this.changeTitle();
+    this.getNewInvestments();
   }
 
   changeTitle(): void {
     this.titleService.setTitle('Inicio - Socios360');
+  }
+
+  getNewInvestments(): void {
+    this.homeService.getNewInvestments().subscribe(newInvOffers => {
+      if(newInvOffers){
+        this.newInvOffers = newInvOffers;
+        console.log(this.newInvOffers);
+      } else {
+        // error en traer las ultimas ofertas de inversión
+      }
+    });
+  }
+
+  calculateFinancingPercentage(currentFinancing: number, financingTarget: number): number {
+    const result: number = (currentFinancing/financingTarget);
+    return parseFloat((result * 100).toFixed(2));
   }
 
 }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { INVESTMENTOFFERS } from '../data/database';
+import { InvestmentOffer } from '../data/interfaces/investmentOffer';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,21 @@ import { Injectable } from '@angular/core';
 export class HomeService {
 
   constructor() { }
+
+  getNewInvestments(): Observable<InvestmentOffer[]> {
+    let end: number = 3;
+    let newInvestments: InvestmentOffer[] = [];
+    if(INVESTMENTOFFERS.length > 2){
+      for(let i = (INVESTMENTOFFERS.length - 1); i >= (INVESTMENTOFFERS.length - 3); i--){
+        if(end > 0) {
+          newInvestments.push(INVESTMENTOFFERS[i]);
+          end--;
+        } else {
+          return of(newInvestments);
+        }
+      }
+    }
+    return of(newInvestments);
+  }
+
 }
